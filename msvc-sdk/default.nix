01ed8caf3a46657db49fced4f49018c5703f2059
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-07-16 22:48:19 -0400

    Clean up MSVC SDK derivatoin

--- msvc-sdk/default.nix
+++ msvc-sdk/default.nix
@@ -1,11 +1,14 @@
 {
-  stdenv,
+  lib,
   runCommand,
   xwin,
 }:
-assert stdenv.hostPlatform.isx86_64;
-runCommand "msvc-sdk"
+let
+  version = (builtins.fromJSON (builtins.readFile ./manifest.json)).info.productSemanticVersion;
+in
+runCommand "msvc-sdk-${version}"
   {
+    inherit version;
     buildInputs = [ xwin ];
 
     outputHashAlgo = "sha256";
@@ -13,14 +16,23 @@ runCommand "msvc-sdk"
     outputHash = "sha256-2MfUJDwScir/V9+aCu94RYPexSEKGemn92ZNclfYNIw=";
 
     manifest = ./manifest.json;
+
+    meta = {
+      description = "MSVC SDK and Windows CRT for cross compiling on Linux";
+      homepage = "https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/";
+      license = lib.licenses.unfreeRedistributable;
+      maintainers = [ lib.maintainers.RossSmyth ];
+      platforms = lib.intersectLists lib.platforms.linux lib.platforms.x86_64;
+    };
   }
   ''
-    xwin --accept-license --cache-dir=xwin-out --manifest=$manifest splat --preserve-ms-arch-notation --use-winsysroot-style
-    mkdir $out/
-    mv xwin-out/splat/* $out/
+    xwin --accept-license --cache-dir=xwin-out --manifest="$manifest" splat --preserve-ms-arch-notation --use-winsysroot-style
+    mkdir "$out"
+    mv xwin-out/splat/* "$out"
 
+    ls -al "$out"
     echo "Fixing directory names..."
-    pushd $out/Windows\ Kits/10/
+    pushd "$out/Windows Kits/10/"
     mv include Include
     mv lib Lib
     popd
