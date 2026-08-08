bc07aed87505d6567aab4b256c45abcb9a291285
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-08 17:45:04 -0400

    appimagetool: init at 1.9.1

--- default.nix
+++ default.nix
@@ -18,6 +18,8 @@ pkgs.lib.makeScope pkgs.newScope (
     inherit (self) callPackage;
   in
   {
+    appimagetool = callPackage ./appimagetool { };
+
     audiomoth-config = callPackage ./audiomoth-config { };
     audiomoth-flash = callPackage ./audiomoth-flash { };
 
