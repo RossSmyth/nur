82f470f1b886b059b29b1ff87946e3eab8ff4008
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-09-15 22:37:36 -0400

    moon-illusion: init

--- default.nix
+++ default.nix
@@ -61,5 +61,6 @@ pkgs.lib.makeScope pkgs.newScope (
     two-kinds-of-people = callPackage ./twokindsofpeople { };
     melt = callPackage ./melt { };
     moonlight-duelists = callPackage ./moonlight-duelists { };
+    moon-illusion = callPackage ./moon-illusion { };
   }
 )
